9c86395749bbe768c79c25aefbe1c3aff3ed43ed
Ajx <jayxx130@gmail.com>
2025-07-23 00:43:29 +0530

    react-router

---
+++ react-router/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>hellow</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
